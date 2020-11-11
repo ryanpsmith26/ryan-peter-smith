127caf25271c34282eb406fa73883461145b58aa
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-11-11 16:43:09 -0500

    adds contact and social links in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import styles from './footer.module.css';
 
@@ -17,7 +18,18 @@ const Footer = () => {
 				</button>
 			</div>
 			<div className={styles.footerDiv}>
-				<p className={styles.footerText}>RPS Designs</p>
+				<a href="mailto: ryanpsmith01@gmail.com">
+					<FontAwesomeIcon icon={faEnvelope} />
+				</a>
+				<a href="https://www.linkedin.com/in/ryan-peter-smith/" target="_blank">
+					<FontAwesomeIcon icon={faLinkedin} />
+				</a>
+				<a href="https://github.com/ryanpsmith26" target="_blank">
+					<FontAwesomeIcon icon={faGithub} />
+				</a>
+				<a href="https://twitter.com/ryanpsmith26" target="_blank">
+					<FontAwesomeIcon icon={faTwitter} />
+				</a>
 			</div>
 		</footer>
 	);
