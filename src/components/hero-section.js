c7df985dff5bfb3a9ca55fe1f69028030d8e2674
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-11-12 09:27:35 -0500

    refactors hero image to utilize gatsby background images

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
+import { graphql, StaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
 import styles from './hero-section.module.css';
 
 const HeroSection = () => {
 	return (
-		<React.Fragment>
-			<img
-				src={require('../images/outlier-wash-crop.jpeg')}
-				className={styles.heroImg}
-				alt="dev gazing out window"
-				loading="eager"
-			/>
-			<div className={styles.heroTextDiv}>
-				<h1>Ryan P. Smith</h1>
-				<Typewriter
-					options={{
-						strings: [ 'Software Engineer', 'Maker', 'Home Brewer', 'Cook' ],
-						autoStart: true,
-						loop: true
-					}}
-				/>
-			</div>
-		</React.Fragment>
+		<StaticQuery
+			query={graphql`
+				query {
+					desktop: file(relativePath: { eq: "outlier-wash-crop.jpeg" }) {
+						childImageSharp {
+							fluid(quality: 90, maxWidth: 1920) {
+								...GatsbyImageSharpFluid_withWebp
+							}
+						}
+					}
+				}
+			`}
+			render={(data) => {
+				const imageData = data.desktop.childImageSharp.fluid;
+				return (
+					<BackgroundImage
+						Tag="section"
+						className={styles.heroImg}
+						fluid={imageData}
+						backgroundColor={`#040e18`}
+					>
+						<div className={styles.heroTextDiv}>
+							<h1>Ryan P. Smith</h1>
+							<Typewriter
+								options={{
+									strings: [ 'Software Engineer', 'Maker', 'Home Brewer', 'Cook' ],
+									autoStart: true,
+									loop: true
+								}}
+							/>
+						</div>
+					</BackgroundImage>
+				);
+			}}
+		/>
 	);
 };
 
