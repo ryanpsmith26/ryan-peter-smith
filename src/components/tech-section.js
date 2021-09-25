6ae05896f0f72e2236db40b098cd83a4b25061a4
ryanpsmith26 <ryanpsmith01@gmail.com>
2021-09-25 10:32:56 -0500

    chore: swap react logo, grow node

--- src/components/tech-section.js
+++ src/components/tech-section.js
@@ -17,15 +17,15 @@ const TechSection = () => {
 						<img src={require('../images/html.svg')} alt="HTML5 Logo" className={styles.techLogoSm} />
 						<img src={require('../images/css.svg')} alt="CSS3 Logo" className={styles.techLogoSm} />
 						<img src={require('../images/javascript.svg')} alt="JavaScript Logo" className={styles.techLogoMd} />
-						<img src={require('../images/nodejs.svg')} alt="Node.js Logo" className={styles.techLogoLg} />
-						<img src={require('../images/react4.svg')} alt="React Logo" className={styles.techLogoMd} />
+						<img src={require('../images/nodejs.svg')} alt="Node.js Logo" className={styles.techLogoXl} />
+						<img src={require('../images/react7.svg')} alt="React Logo" className={styles.techLogoLg} />
 						<img src={require('../images/redux.svg')} alt="Redux Logo" className={styles.techLogoMd} />
 						<img src={require('../images/styled-components.svg')} alt="styled-components Logo" className={styles.techLogoLg} />
 						<img src={require('../images/postgresql.svg')} alt="PostgreSQL Logo" className={styles.techLogoMd} />
 						<img src={require('../images/firebase.svg')} alt="Firebase Logo" className={styles.techLogoMd} />
 						<img src={require('../images/aws-lambda-1.svg')} alt="AWS Lambdas Logo" className={styles.techLogoSm} />
-						<img src={require('../images/vercel.svg')} alt="Vercel Logo" className={styles.techLogoXs} />
 						<img src={require('../images/storybook.svg')} alt="Storybook Logo" className={styles.techLogoXs} />
+						<img src={require('../images/vercel.svg')} alt="Vercel Logo" className={styles.techLogoXs} />
 				</Marquee>
 			</div>
 		</div>
