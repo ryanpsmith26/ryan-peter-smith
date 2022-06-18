78a7ff7d43599744ac120fab6f700d83142a4466
ryanpsmith26 <ryanpsmith01@gmail.com>
2022-06-18 07:30:39 -0600

    update theme colors to match site

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
 				name: `gatsby-starter-default`,
 				short_name: `starter`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				background_color: `#333333`,
+				theme_color: `#333333`,
 				display: `minimal-ui`,
 				icon: `src/images/logo/lightgray.png`
 			}
